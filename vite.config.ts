32ad1a106d36e32d482809f4537634e34f1222fc
will <35718723+z9wen@users.noreply.github.com>
2025-09-07 12:04:53 +1000

    Comment out server settings in vite.config.ts
    Comment out server configuration in Vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // base: '/bt2magnet/',
-  server: {
-    port: 3000,
-  }
+  // server: {
+  //   port: 3000,
+  // }
 })
